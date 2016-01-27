ea2ee13c098dfba258a871298db717043df70035
OHTA Shogo <athos0220@gmail.com>
2016-01-27 23:22:25 +0900

    Implement basic sprite rendering

--- src/mario_brojures/core.cljs
+++ src/mario_brojures/core.cljs
@@ -3,9 +3,27 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(def load-count (atom 0))
+(def imgs-to-load 4)
+(def level-width 2400)
+(def level-height 256)
 
+(def sprite-params
+  {:small-player
+   {:standing {:img-src "mario-small.png"
+               :src-offset [0 0]
+               :frame-size [16 16]
+               :bbox-offset [3 1]
+               :bbox-size [11 15]}}})
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn make-from-params [params]
+  (let [img (.createElement js/document "img")]
+    (set! (.-src img) (str "sprites/" (:img-src params)))
+    {:params params
+     :img img
+     }))
 
+(defn render [context sprite dx dy]
+  (let [{[sx sy] :src-offset, [sw sh] :frame-size} (:params sprite)]
+    (.drawImage context (:img sprite) sx sy sw sh dx dy sw sh)))
 
